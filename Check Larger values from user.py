0cebd4feb2cf7785e18355b5dccf9fbdad4b9b11
Ashayeshabh <ashay.c.4928@iibm.in>
2025-12-16 18:00:58 +0530

    Add files via upload

---
+++ Check Larger values from user.py
@@ -0,0 +1,8 @@
+x=int(input("enter first value"))
+y=int(input("enter second value"))
+if x>y:
+    print("first value is larger")
+if y>x:
+    print("second value is larger")
+if x==y:
+    print("both values are equal")
