1063610da3892a9f499d7c1ef18e790ab1434bf1
Ashayeshabh <ashay.c.4928@iibm.in>
2025-12-16 17:50:55 +0530

    Add files via upload

---
+++ Smallest value from three user input.py
@@ -0,0 +1,11 @@
+a=int(input("enter the first value"))
+b=int(input("enter the second value"))
+c=int(input("enter the third value"))
+if a<b and a<c:
+    print("a is smaller")
+if b<c and b<c:
+    print("b is smaller")
+if c<a and c<b:
+    print("c is smaller")
+if a==b==c:
+    print("All the three values are equal")
