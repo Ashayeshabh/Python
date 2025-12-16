ffc8aa4ea02510dbb66841f5e09d1b7346268970
Ashayeshabh <ashay.c.4928@iibm.in>
2025-12-16 17:38:40 +0530

    Add files via upload

---
+++ Simple Interest.py
@@ -0,0 +1,7 @@
+def calc():
+    p=float(input("enter the principal value"))
+    r=float(input("enter the rate value"))
+    t=float(input("enter the time value"))
+    si=(p*r*t)/100
+    print("The simple interest is",si)
+calc()    
