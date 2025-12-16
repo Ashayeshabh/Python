4da90f6f632f0fd5007147d20253e7edad832840
Ashayeshabh <ashay.c.4928@iibm.in>
2025-12-16 17:46:23 +0530

    Add files via upload

---
+++ Name of Days.py
@@ -0,0 +1,17 @@
+x = int(input("Enter a value from 1-7 to see the name of day"))
+if x==1:
+    print("Sunday")
+if x==2:
+    print("Monday")
+if x==3:
+    print("Tuesday")
+if x==4:
+    print("Wednesday")
+if x==5:
+    print("Thursday")
+if x==6:
+    print("Friday")
+elif x==7:
+    print("Saturday")
+else:
+    print("Go to hell")
