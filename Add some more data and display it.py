2b0b8058dbc7374d285b9ba8e8d4c29a545bd54e
Ashay Eshabh <ashay.c.4928@iibm.in>
2026-02-08 09:53:51 +0530

    Delete Add some more data and display it.py

--- Add some more data and display it.py
+++
@@ -1,8 +0,0 @@
-import os
-if os.path.exists("sample.txt"):
-    os.remove("sample.txt")
-else:
-    ("file doesn't exists")    
-file1=open("sample.txt","w")
-file1.write("Hello World")
-file1.close()
